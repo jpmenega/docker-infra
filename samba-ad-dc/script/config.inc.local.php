4b5b4bc957ac17f0bd5089e548868eaacfdb4bdd
Joao Paulo Menegatti <jpmenegatti@hotmail.com>
2021-04-12 16:45:03 +0000

    Samba

--- samba-ad-dc/script/config.inc.local.php
+++ samba-ad-dc/script/config.inc.local.php
@@ -5,6 +5,7 @@ $ldap_bindpw = "secret";
 $ldap_base = "dc=example,dc=com";
 
 $ldap_login_attribute = "cn";
+$ldap_filter = "(&(objectClass=person)($ldap_login_attribute={login}))";
 $ad_mode = true;
 $ad_options['force_unlock'] = true;
 $ad_options['change_expired_password'] = true;
@@ -13,4 +14,14 @@ $use_questions = false;
 $use_sms = false;
 $lang = "pt-BR";
 
+$mail_address_use_ldap = true;
+$notify_on_change = true;
+$mail_smtp_auth = true;
+
+$mail_from = "admin@example.com";
+$mail_from_name = "Self Service Password";
+$mail_smtp_host = 'localhost';
+$mail_smtp_user = '';
+$mail_smtp_pass = '';
+$mail_smtp_port = 25;
 ?>
